c6ecc5846f5445e3f0a1086618f3e09394501b06
utilitron000 <utilitron@proton.me>
2025-02-11 14:07:38 -0800

    ??? Not sure. We'll see.

--- init.lua
+++ init.lua
@@ -933,7 +933,7 @@ require('lazy').setup({
   --
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
+  --  require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
@@ -972,3 +972,6 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+--
+vim.o.conceallevel = 1
--- lua/custom/plugins/obsidian.nvim.lua
+++ lua/custom/plugins/obsidian.nvim.lua
@@ -27,4 +27,16 @@ return {
 
     -- see below for full list of options 👇
   },
+  keys = {
+    { '<leader>on', '<cmd>ObsidianNew<cr>', desc = 'New Obsidian note', mode = 'n' },
+    { '<leader>oo', '<cmd>ObsidianSearch<cr>', desc = 'Search Obsidian notes', mode = 'n' },
+    { '<leader>ob', '<cmd>ObsidianBacklinks<cr>', desc = 'Show location list of backlinks', mode = 'n' },
+    { '<leader>ot', '<cmd>ObsidianTemplate<cr>', desc = 'Follow link under cursor', mode = 'n' },
+  },
+
+  templates = {
+    folder = 'templates',
+    date_format = 'YYYY-MM-dd',
+    time_format = '%H:%M',
+  },
 }
